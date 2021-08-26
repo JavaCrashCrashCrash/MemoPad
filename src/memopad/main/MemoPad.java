f4b28c2f0b48bc79c4366754380f522df0411a90
Kimmoonsu <visitant2@naver.com>
2021-08-26 20:02:33 +0900

    폴더 만드는 방법

--- src/memopad/main/MemoPad.java
+++ src/memopad/main/MemoPad.java
@@ -1,5 +1,7 @@
 package memopad.main;
 
+import java.io.File;
+
 import com.memopad.model.Folder;
 import com.memopad.model.FolderManager;
 import com.memopad.model.Memo;
@@ -7,25 +9,42 @@ import com.memopad.model.Memo;
 public class MemoPad {
 	public static void main(String[] args) {
 //		ButtonController buttonController = new ButtonController();
-		FolderManager folderManager = new FolderManager();
+//		FolderManager folderManager = new FolderManager();
+//		
+//		
+//		Folder folder = new Folder("Fold", "C://users/jinseo/Desktop/");
+//		Memo memo = new Memo("hello", "hello My name is Jinseo");
+//		Memo memo2 = new Memo("bye", "bye bye");
+//		folder.addMemo(memo);
+//		folder.addMemo(memo2);
+//		folderManager.addFolder(folder);
+//		
+//		folder = new Folder("Fold2", "C://");
+//		memo = new Memo("hi", "hi jinseo");
+//		memo2 = new Memo("goodbye", "goodbye jinseo");
+//		folder.addMemo(memo);
+//		folder.addMemo(memo2);
+//		
+//		folderManager.addFolder(folder);
+//		
+//		
+//		folderManager.printFolderAll();
 		
 		
-		Folder folder = new Folder("Fold", "C://users/jinseo/Desktop/");
-		Memo memo = new Memo("hello", "hello My name is Jinseo");
-		Memo memo2 = new Memo("bye", "bye bye");
-		folder.addMemo(memo);
-		folder.addMemo(memo2);
-		folderManager.addFolder(folder);
 		
-		folder = new Folder("Fold2", "C://");
-		memo = new Memo("hi", "hi jinseo");
-		memo2 = new Memo("goodbye", "goodbye jinseo");
-		folder.addMemo(memo);
-		folder.addMemo(memo2);
-		
-		folderManager.addFolder(folder);
-		
-		
-		folderManager.printFolderAll();
+		String path = "/Users/Kimmoonsu/Desktop/MemoFolder"; // 폴더 경로
+		File folder = new File(path);
+
+		// 해당 디렉토리가 없을경우 디렉토리를 생성합니다.
+		if (!folder.exists()) {
+			try {
+				folder.mkdir(); // 폴더 생성합니다.
+				System.out.println("폴더가 생성되었습니다.");
+			} catch (Exception e) {
+				e.getStackTrace();
+			}
+		} else {
+			System.out.println("이미 폴더가 생성되어 있습니다.");
+		}
 	}
 }
